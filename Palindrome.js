bffd8bfcac9c353b56cc682203c314120ba2f32e
ShaquibKhanhub <ShaquibKhanrdx34@gmail.com>
2022-09-14 15:07:08 +0530

    added is Prime & is Palindrome

---
+++ Palindrome.js
@@ -0,0 +1,19 @@
+let N = 6;
+let str = "nrupul" 
+
+let bag=""
+
+for(let i = N-1;i>=0;i--){
+
+          bag+=str[i];
+
+}
+if(str==bag){
+
+         console.log("Yes")
+
+    }else{
+
+        console.log("No");
+
+     }
